40dbb2415ce367904e0631447083c9752113ee05
Youssef Lasheen <youssef@lasheen.dev>
2025-11-28 16:16:12 +0200

    upd

--- signaling-server/bin/main.dart
+++ signaling-server/bin/main.dart
@@ -5,17 +5,18 @@ import 'package:dart_firebase_admin/messaging.dart';
 import 'package:signaling_server/server.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
-const _defineHost = String.fromEnvironment('HOST');
+// const _defineHost = String.fromEnvironment('HOST');
 const _definePort = String.fromEnvironment('PORT');
 const _defineFirebaseProjectId = String.fromEnvironment('FIREBASE_PROJECT_ID');
 const _defineServiceAccountFile =
     String.fromEnvironment('FIREBASE_SERVICE_ACCOUNT_FILE');
 
 void main(List<String> args) async {
-  if (_defineHost.isEmpty ||
+  if (
+      // _defineHost.isEmpty ||
       _definePort.isEmpty ||
-      _defineFirebaseProjectId.isEmpty ||
-      _defineServiceAccountFile.isEmpty) {
+          _defineFirebaseProjectId.isEmpty ||
+          _defineServiceAccountFile.isEmpty) {
     stdout.writeln(
       'Warning: Required environment variables not configured. FCM notifications are disabled.',
     );
@@ -27,10 +28,10 @@ void main(List<String> args) async {
     );
     return;
   }
-  final host = _defineHost;
+  // final host = _defineHost;
   final portEnv = _definePort;
   final port = int.tryParse(portEnv) ?? 8080;
-  final resolvedHost = InternetAddress(host);
+  final resolvedHost = InternetAddress.anyIPv4;
 
   final firebaseProjectId = _defineFirebaseProjectId;
   final firebaseCredentialsPath = _defineServiceAccountFile;
